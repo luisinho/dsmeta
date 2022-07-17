b2034e76535f99b47c45d44705eb14319b98a8f1
luis3977@gmail.com <luis3977@gmail.com>
2022-07-17 20:13:28 -0300

    Envio email

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,12 +3,17 @@ package com.devsuperior.dsmeta.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.EmailService;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
@@ -18,11 +23,20 @@ public class SaleController {
 	@Autowired
 	private SaleService saleService;
 
+	@Autowired
+	private EmailService emailService;
+
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
 			Pageable pageable) {
 		return this.saleService.findSales(minDate, maxDate, pageable);
+	}
+
+	@GetMapping("/{id}/notification")
+	public ResponseEntity<String> notifyEmail(@PathVariable Long id) {
+		String msg = this.emailService.sendEmail(id);
+		return ResponseEntity.ok(msg);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/EmailService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public String sendEmail(Long saleId) {
+
+		Sale sale = this.saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+		String msg = "Vendedor " + sale.getSellerName() + "foi destaque em " + date
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		
+		return msg;		
+		
+	}
+}
