9825f4ef4bbe91281c9adabf9cc312fe31b6e95a
luis3977@gmail.com <luis3977@gmail.com>
2022-07-16 23:39:32 -0300

    Other components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Header from './components/Header';
 import NotificationButton from './components/NotificationButton';
+import SalesCard from './components/SalesCard';
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
 
-export default App
+export default App;
