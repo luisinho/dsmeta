5d3df0d971b8e4f12b3f4509cc1764ade7c1e37d
luis3977@gmail.com <luis3977@gmail.com>
2022-07-17 20:14:58 -0300

    Send notification.

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId: number) {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`)
+        .then(response => {
+            console.log(response);
+        });
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-bnt">
+        <div className="dsmeta-red-bnt" onClick={() => { handleClick(saleId) }}>
             <img src={icon} alt="Notificar" />
         </div>
     );
