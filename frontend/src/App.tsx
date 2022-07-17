2552ef1dbee4aecabf9729dfba480f0ae4d918a1
luis3977@gmail.com <luis3977@gmail.com>
2022-07-16 22:50:43 -0300

    First component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import NotificationButton from './components/NotificationButton';
 
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   );
 }
 
